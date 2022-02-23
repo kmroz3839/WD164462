c5e1ad7de82f47737ae8dd40b83183746345cf90
counter185 <33550839+counter185@users.noreply.github.com>
2022-02-23 15:10:24 +0100

    ważne zmiany

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-import sys
+import sys, math
 
 #ta funkcja printuje wersję
 print(sys.version)
 
+a = 123
+b = 23
+
+d = (2+7j)
+print(type(d))
+
+print(d)
+
+h = 7
+i = 2
+
+i **= 2
+print(i)
+
+print("wynik %(z1)d - %(z2)d = %(z3)d" %{'z1': a, 'z2': b, 'z3': a-b})
+#jak to możliwe /// /? / ? ?/ ?/ ?? ? ?/ s
