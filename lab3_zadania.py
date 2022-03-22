cf42ac304ee8b37ccdba849cd9e8fc788e6f2dc7
counter185 <33550839+counter185@users.noreply.github.com>
2022-03-22 18:40:29 +0100

    Update lab3_zadania.py

--- lab3_zadania.py
+++ lab3_zadania.py
@@ -14,15 +14,8 @@ for x in range(0,10):
     lista1.append(random.randint(0,300))
 lista2 = [x for x in lista1 if x % 2 == 0]
 
-#z3
-z3_sl = {
-"chleb": "szt",
 
-    }
-#!
-
-
-#4
+#z4
 def trojkat_prostokatny(a, b, c):
     z4l_a = [a,b,c]
     for x in range(0,3):
@@ -30,8 +23,27 @@ def trojkat_prostokatny(a, b, c):
             return True
     return False
 
-#5
+#z5
 def pole_trapezu(a = 2,b = 2,h = 2):
     return ((a+b)*h)/2
 
 print(pole_trapezu())
+
+#z6
+def iloczyn_el_c(a=1, b=4, ile=10):
+    r = a
+    for x in range(0,10):
+        r *= r*(b**x)
+    return ret
+
+#z7
+# ??????
+
+#z8
+def il_c(lista):
+    ilosc = len(lista)
+    wartosc = 0;
+    for x in lista:
+        wartosc += lista[x]
+    return (ilosc, wartosc) #???
+
